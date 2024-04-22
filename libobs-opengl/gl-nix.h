3bd3f34b7e94a6d623815cad40f979dec8b14eee
Kurt Kartaltepe <kkartaltepe@gmail.com>
2024-04-21 20:32:48 -0700

    PERF: Cursed gs_present deferment, glorious perf.

--- libobs-opengl/gl-nix.h
+++ libobs-opengl/gl-nix.h
@@ -49,6 +49,7 @@ struct gl_winsys_vtable {
 	void (*device_load_swapchain)(gs_device_t *device, gs_swapchain_t *swap);
 
 	void (*device_present)(gs_device_t *device);
+	void (*device_present_async)(gs_device_t *device, struct gl_windowinfo *wi);
 
 	struct gs_texture *(*device_texture_create_from_dmabuf)(gs_device_t *device, unsigned int width,
 								unsigned int height, uint32_t drm_format,
